8c59e7563a973dd48d8e054fb3d0487a629db640
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2023-10-25 17:49:29 -0230

    Adds a form to the booking page

--- src/components/BookingPage.jsx
+++ src/components/BookingPage.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const BookingPage = () => {
-  return <div>BookingPage</div>;
+  return (
+    <main id="content">
+      <section>
+        <div>
+          {/* Form layout is currently placeholder and does not match our planned layout. */}
+          <form action="">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+            <br />
+            <label htmlFor="addr">Address</label>
+            <input type="text" id="addr" />
+            <br />
+            {/* Consider validating these fields later */}
+            <label htmlFor="cardNum">Credit Card #</label>
+            <input type="number" id="cardNum" />
+            <br />
+            <label htmlFor="expiry">Expiry Date</label>
+            <input type="date" />
+            <label htmlFor="secCode">CSV</label>
+            <input type="number" min="100" max="999" />
+          </form>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default BookingPage;
